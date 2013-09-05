93b82769c11a5f2e46aeebb8d9e84f761336cca4
kangpangpang <kangpangpang@gmail.com>
2013-09-05 13:47:37 +0800

    U: update output file name

--- bin/weblint.js
+++ bin/weblint.js
@@ -10,31 +10,36 @@ var args = process.argv.slice(2);
 var cwd = process.cwd()
 
 async.eachSeries(args, function(arg, cb) {
-    try {
-        var plugin = require('../lib/' + arg)
-        plugin(cwd, function(err, result) {
-            output(arg, result)
-            if (!err) {
-              cb(err)
-            } else {
-              cb()
-            }
-        })
-    }catch(e) {
-        console.error('error plugin ' + arg, e)
-        cb('load plugin ' + arg + ' error!')
-    }
+  try {
+    var plugin = require('../lib/' + arg)
+    plugin(cwd, function(err, result, filename) {
+      output(arg, result, filename)
+      if (!err) {
+        cb(err)
+      } else {
+        cb()
+      }
+    })
+  }catch(e) {
+    console.error('error plugin ' + arg, e)
+    cb('load plugin ' + arg + ' error!')
+  }
 
 }, function(err) {
     if (err) {
-        console.info('totoro check error!' + err)
-        process.exit(1)
+      console.info('totoro check error!' + err)
+      process.exit(1)
     } else {
-        process.exit(0)
+      process.exit(0)
     }
 })
 
-function output(name, result) {
+function output(name, result, filename) {
   mkdir('-p', '../target')
-  fs.writeFileSync('../target/' + name + '.txt', result)
+  if (filename) {
+    name = filename
+  } else {
+    name = name + '.txt'
+  }
+  fs.writeFileSync('../target/' + name, result)
 }
