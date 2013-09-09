c9e91e4a8469085edda0f4b0017853fa06a31948
noahlu <noahseo@gmail.com>
2013-09-09 15:46:16 +0800

    update catbus plugin

---
+++ config/catbus-config.js
@@ -0,0 +1,28 @@
+var config = {
+  options: {
+    "html-doctype": true,
+    "html-hard-code": true, 
+    "html-https-warning": true, 
+    "html-id-duplicate": false, 
+    "html-inline-script": true,
+    "html-meta-charset": false, 
+    "html-quote-value": true,
+    "html-tag-close": false, 
+    "html-unsafe-resource": true, 
+    "js-bitwise": true,
+    "js-debug": true,
+    "js-loopfunc": true,
+    "js-newcap": false,
+    "js-undef": false,
+    "js-unused": false
+  }, 
+  ignore: [
+    '\\.html',
+    '\\.htm'
+  ],
+  require: [
+    'catbus-html-typos'
+  ]
+}
+
+exports.config = config;
